dbba82b40dbfe4c08e46451c3df66434be8c36d3
Adrian Pantea <adrianpantea10@gmail.com>
2021-11-07 22:26:17 +0000

    installed semantic ui react

--- weather-app/src/Components/Home.js
+++ weather-app/src/Components/Home.js
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { WiSunrise } from "react-icons/wi";
 import { WiSunset } from "react-icons/wi";
+import { Container, Divider, Grid, Header, Segment } from "semantic-ui-react";
 
 const Home = () => {
   const api = {
     key: "143d8b76759d409186be983f59e673a1",
     base: "https://api.openweathermap.org/data/2.5/",
   };
-
-  // let url = "http://openweathermap.org/img/wn/";
-  // let png = '@2x.png'
 
   const [query, setQuery] = useState("");
   const [weather, setWeather] = useState({});
@@ -75,11 +73,29 @@ const Home = () => {
 
   return (
     <main>
-      <div className="search-box">
+      <Header as="h1" id="display">
+        <h1 id="app-title">Weather App</h1>
+        <div className="search-box">
+          <button className="btn-search">
+            <i className="fas fa-search"></i>
+          </button>
+          <input
+            type="text"
+            className="input-search"
+            placeholder="search city..."
+            onChange={(e) => {
+              setQuery(e.target.value); // get the value of input typed in,
+            }}
+            value={query} // find the value equal to query
+            onKeyPress={searchWeather}
+          />
+        </div>
+      </Header>
+      {/* <div className="search-box">
         <input
           type="text"
           className="search-bar"
-          placeholder="City..."
+          placeholder="Search the city..."
           onChange={(e) => {
             setQuery(e.target.value); // get the value of input typed in,
           }}
@@ -87,35 +103,57 @@ const Home = () => {
           onKeyPress={searchWeather}
         />
         <i className="fas fa-search svg"></i>
-      </div>
+      </div> */}
+
       {/* <div>{new Date().toLocaleTimeString()}</div> */}
+
+      <Divider />
 
       {typeof weather.main != "undefined" ? (
         <>
-          <div >
-            <div className="weather-container">
+          <Container className="container">
+            <div id="top-weather-section">
               <div>
                 <div className="country-name">
                   {weather.name}, {weather.sys.country}
                 </div>
-                <div className="today-date">{weatherCurrentTime(new Date())}</div>
+                <div className="today-date">
+                  {weatherCurrentTime(new Date())}
+                </div>
               </div>
 
-              <div className="weather-box">
-                <div className="temperature">
-                  <div className="degrees">
-                  {Math.round(weather.main.temp)} <span id="celsius">°C</span></div>
-                  <img src={iconURL} className="weather-icon" alt="" />
+              <div className="top-right-section">
+                <div id="display">
+                  <WiSunrise id="sunrise" />
+                  {new Date(
+                    weather.sys.sunrise * 1000
+                  ).toLocaleTimeString()}{" "}
                 </div>
-
-                
-
-                <div className="weather-description">
-                  {weather.weather[0].description}
+                <div id="display">
+                  <WiSunset id="sunset" />
+                  {new Date(weather.sys.sunset * 1000).toLocaleTimeString()}
                 </div>
+              </div>
+            </div>
 
-                <div>Feels like {Math.round(weather.main.feels_like)}</div>
-                
+            {/* <div> */}
+            <div className="temperature">
+              <div>
+                <div className="degrees">
+                  {Math.round(weather.main.temp)} <span id="celsius">°C</span>
+                </div>
+                <div className="feels">
+                  Feels {Math.round(weather.main.feels_like)}°C
+                </div>
+              </div>
+              <img src={iconURL} className="weather-icon" alt="" />
+            </div>
+
+            <Grid columns={3} divided>
+              <Grid.Row>
+                <Grid.Column>
+                  <div>{weather.weather[0].description}</div>
+                </Grid.Column>
                 <div>
                   Minimum temperature {Math.round(weather.main.temp_min)}{" "}
                 </div>
@@ -123,32 +161,25 @@ const Home = () => {
                 <div>
                   Maximum temperature {Math.round(weather.main.temp_max)}{" "}
                 </div>
+              </Grid.Row>
 
-                <div>Pressure {Math.round(weather.main.pressure)} hPa</div>
+              <Grid.Row>
+                <Grid.Column>
+                  <div>Pressure {Math.round(weather.main.pressure)} hPa</div>
+                </Grid.Column>
+                <Grid.Column>
+                  <div>Humidity {Math.round(weather.main.humidity)}%</div>
+                </Grid.Column>
 
-                <div>Humidity {Math.round(weather.main.humidity)}%</div>
-
-                <div className="weather-wind-container">
-                  <div>Wind {Math.round(weather.wind.speed)}km/hr </div>
-                </div>
-                {/* <br /> */}
-
-                <div id="display-flex">
-                  <WiSunrise id="sunrise" />
-                  {new Date(
-                    weather.sys.sunrise * 1000
-                  ).toLocaleTimeString()}{" "}
-                </div>
-
-                {/* <br /> */}
-                <div id="display-flex">
-                  <WiSunset id="sunset" />
-
-                  {new Date(weather.sys.sunset * 1000).toLocaleTimeString()}
-                </div>
-              </div>
-            </div>
-          </div>
+                <Grid.Column>
+                  <div className="weather-wind-container">
+                    <div>Wind {Math.round(weather.wind.speed)}km/hr </div>
+                  </div>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+            {/* </div> */}
+          </Container>
         </>
       ) : (
         ""
