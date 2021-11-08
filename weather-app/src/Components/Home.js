605dc83b538eebaad99b579449898769a8e9d987
Adrian Pantea <adrianpantea10@gmail.com>
2021-11-08 16:53:46 +0000

    further styling

--- weather-app/src/Components/Home.js
+++ weather-app/src/Components/Home.js
@@ -1,19 +1,11 @@
 import React, { useEffect, useState } from "react";
-import { WiSunrise } from "react-icons/wi";
-import { WiSunset } from "react-icons/wi";
+import { Container, Divider, Grid, Header } from "semantic-ui-react";
+
+import { WiSunrise, WiSunset, WiHumidity, WiBarometer } from "react-icons/wi";
 import { BsCloudSunFill } from "react-icons/bs";
-import { WiHumidity, WiBarometer } from "react-icons/wi";
 import { FiWind } from "react-icons/fi";
 import { FaTemperatureLow, FaTemperatureHigh } from "react-icons/fa";
 import { GoReport } from "react-icons/go";
-import {
-  Container,
-  Divider,
-  Grid,
-  Header,
-  Segment,
-  Image,
-} from "semantic-ui-react";
 
 const Home = () => {
   const api = {
@@ -103,7 +95,22 @@ const Home = () => {
         </div>
         <div id="display-flex">
           <h1 id="app-title">Weather App</h1>
-          <BsCloudSunFill className="logo" />
+          {/* <BsCloudSunFill className="logo" /> */}
+          <svg
+            stroke="currentColor"
+            fill="currentColor"
+            strokeWidth="0"
+            viewBox="0 0 16 16"
+            className="logo"
+            height="1em"
+            width="1em"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            
+            <path d="M11.473 11a4.5 4.5 0 0 0-8.72-.99A3 3 0 0 0 3 16h8.5a2.5 2.5 0 0 0 0-5h-.027z"></path>
+            {/* <animate attributeName="transform" type="rotate" begin="0s" dur="10s" from="0 200 200" to="360 400 400" repeatCount="indefinite" color="yellow"/> */}
+            <path d="M10.5 1.5a.5.5 0 0 0-1 0v1a.5.5 0 0 0 1 0v-1zm3.743 1.964a.5.5 0 1 0-.707-.707l-.708.707a.5.5 0 0 0 .708.708l.707-.708zm-7.779-.707a.5.5 0 0 0-.707.707l.707.708a.5.5 0 1 0 .708-.708l-.708-.707zm1.734 3.374a2 2 0 1 1 3.296 2.198c.199.281.372.582.516.898a3 3 0 1 0-4.84-3.225c.352.011.696.055 1.028.129zm4.484 4.074c.6.215 1.125.59 1.522 1.072a.5.5 0 0 0 .039-.742l-.707-.707a.5.5 0 0 0-.854.377zM14.5 6.5a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1h-1z"></path>
+          </svg>
         </div>
       </Header>
 
@@ -147,7 +154,7 @@ const Home = () => {
               </div>
 
               <div>
-              <img src={iconURL} className="weather-icon" alt="" />
+                <img src={iconURL} className="weather-icon" alt="" />
               </div>
             </div>
 
@@ -191,11 +198,9 @@ const Home = () => {
                     <FiWind id="icons" />
                     {Math.round(weather.wind.speed)}km/hr{" "}
                   </div>
-                  {/* </div> */}
                 </Grid.Column>
               </Grid.Row>
             </Grid>
-            {/* </div> */}
           </Container>
         </>
       ) : (
